9804425540cbaef4c52f0d4fdb8eaf03dbf77ddd
Dennis Law <denn15_law@live.ca>
2022-01-11 06:13:01 +0000

    add validation for params in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :firstName, presence: true
+  validates :lastName, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true
+  validates :password, length: {minimum: 5}
+  validates :password_confirmation, presence: true
+  
+  before_save :case_email
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email.downcase.strip)
+    @user && @user.authenticate(password) ? @user : nil
+  end
+
+  private
+    def case_email  
+      self.email.downcase!
+      self.email.strip
+    end
 end
